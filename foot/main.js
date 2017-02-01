aafa88138fea734b1dd6621ce7b08b01cfde8674
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-02-01 15:44:25 +0100

    Map ZQSD to move camera Removed specular from ground material

--- foot/main.js
+++ foot/main.js
@@ -8,11 +8,23 @@ var BABYLON;
             // Create camera
             var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(15, 15, 15), this.scene);
             camera.attachControl(this.scene.getEngine().getRenderingCanvas());
+            // Map ZQSD keys to move camera
+            camera.keysUp = [90]; // Z
+            camera.keysDown = [83]; // S
+            camera.keysLeft = [81]; // Q
+            camera.keysRight = [68]; // D
             camera.setTarget(new BABYLON.Vector3(0, 0, 0));
             // Create light
             var light = new BABYLON.PointLight("light", new BABYLON.Vector3(1000, 1000, 1000), this.scene);
             // Create scene meshes
             var ground = BABYLON.Mesh.CreateGround("ground", 100, 50, 2, this.scene);
+            // Create standard material
+            var groundMaterial = new BABYLON.StandardMaterial("ground", this.scene);
+            ground.material = groundMaterial;
+            var grassTexture = new BABYLON.Texture("assets/grass.jpg", this.scene, false, false, BABYLON.Texture.NEAREST_SAMPLINGMODE);
+            grassTexture.uScale = grassTexture.vScale = 10.0;
+            groundMaterial.diffuseTexture = grassTexture;
+            groundMaterial.specularColor = BABYLON.Color3.Black(); // new Color3(0, 0, 0);
         };
         return Main;
     }());
--- foot/main.ts
+++ foot/main.ts
@@ -11,6 +11,13 @@ module BABYLON {
             // Create camera
             var camera = new FreeCamera("camera", new Vector3(15, 15, 15), this.scene);
             camera.attachControl(this.scene.getEngine().getRenderingCanvas());
+
+            // Map ZQSD keys to move camera
+            camera.keysUp = [90]; // Z
+            camera.keysDown = [83]; // S
+            camera.keysLeft = [81] // Q
+            camera.keysRight = [68]; // D
+
             camera.setTarget(new Vector3(0, 0, 0));
 
             // Create light
@@ -18,6 +25,16 @@ module BABYLON {
 
             // Create scene meshes
             var ground = Mesh.CreateGround("ground", 100, 50, 2, this.scene);
+
+            // Create standard material
+            var groundMaterial = new StandardMaterial("ground", this.scene);
+            ground.material = groundMaterial;
+
+            var grassTexture = new Texture("assets/grass.jpg", this.scene, false, false, Texture.NEAREST_SAMPLINGMODE);
+            grassTexture.uScale = grassTexture.vScale = 10.0;
+            groundMaterial.diffuseTexture = grassTexture;
+
+            groundMaterial.specularColor = Color3.Black(); // new Color3(0, 0, 0);
         }
     }
 }
