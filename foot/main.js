90193edc149177587d478f958cf910eb44d01fc6
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-02-01 15:16:07 +0100

    Adding light and ground

--- foot/main.js
+++ foot/main.js
@@ -5,7 +5,14 @@ var BABYLON;
             this.scene = scene;
         }
         Main.prototype.main = function () {
-            // Code here :)
+            // Create camera
+            var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(15, 15, 15), this.scene);
+            camera.attachControl(this.scene.getEngine().getRenderingCanvas());
+            camera.setTarget(new BABYLON.Vector3(0, 0, 0));
+            // Create light
+            var light = new BABYLON.PointLight("light", new BABYLON.Vector3(1000, 1000, 1000), this.scene);
+            // Create scene meshes
+            var ground = BABYLON.Mesh.CreateGround("ground", 100, 50, 2, this.scene);
         };
         return Main;
     }());
--- foot/main.ts
+++ foot/main.ts
@@ -8,7 +8,16 @@ module BABYLON {
         }
 
         public main (): void {
-            // Code here :)
+            // Create camera
+            var camera = new FreeCamera("camera", new Vector3(15, 15, 15), this.scene);
+            camera.attachControl(this.scene.getEngine().getRenderingCanvas());
+            camera.setTarget(new Vector3(0, 0, 0));
+
+            // Create light
+            var light = new PointLight("light", new Vector3(1000, 1000, 1000), this.scene);
+
+            // Create scene meshes
+            var ground = Mesh.CreateGround("ground", 100, 50, 2, this.scene);
         }
     }
 }
