97a2f26bec21113d748978db3ceb9f9cee744d1f
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-02-01 17:05:05 +0100

    adding skybox and obstacles

--- foot/main.js
+++ foot/main.js
@@ -15,7 +15,7 @@ var BABYLON;
             camera.keysRight = [68]; // D
             camera.setTarget(new BABYLON.Vector3(0, 0, 0));
             // Create light
-            var light = new BABYLON.PointLight("light", new BABYLON.Vector3(1000, 1000, 1000), this.scene);
+            var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 100, 0), this.scene);
             // Create scene meshes
             var ground = BABYLON.Mesh.CreateGround("ground", 100, 50, 2, this.scene);
             // Create standard material
@@ -24,7 +24,36 @@ var BABYLON;
             var grassTexture = new BABYLON.Texture("assets/grass.jpg", this.scene, false, false, BABYLON.Texture.NEAREST_SAMPLINGMODE);
             grassTexture.uScale = grassTexture.vScale = 10.0;
             groundMaterial.diffuseTexture = grassTexture;
+            groundMaterial.diffuseColor = BABYLON.Color3.Yellow();
             groundMaterial.specularColor = BABYLON.Color3.Black(); // new Color3(0, 0, 0);
+            // Skybox
+            var skybox = BABYLON.Mesh.CreateBox("skybox", 1000, this.scene, false, BABYLON.Mesh.BACKSIDE);
+            var skyboxMaterial = new BABYLON.StandardMaterial("skyboxMaterial", this.scene);
+            skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/TropicalSunnyDay", this.scene);
+            skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+            skyboxMaterial.disableLighting = true;
+            skybox.material = skyboxMaterial;
+            // Create obstacles
+            var leftCube = BABYLON.Mesh.CreateBox("leftCube", 10, this.scene);
+            leftCube.position.x -= ground._width / 2;
+            leftCube.position.y = 5;
+            leftCube.scaling.z = 5;
+            leftCube.scaling.x = 0.1;
+            var rightCube = BABYLON.Mesh.CreateBox("rightCube", 10, this.scene);
+            rightCube.position.x += ground._width / 2; // Same as left cube except +ground._height
+            rightCube.position.y = 5;
+            rightCube.scaling.z = 5;
+            rightCube.scaling.x = 0.1;
+            var backCube = BABYLON.Mesh.CreateBox("backCube", 10, this.scene);
+            backCube.position.z -= ground._height / 2;
+            backCube.position.y = 5;
+            backCube.scaling.x = 10;
+            backCube.scaling.z = 0.1;
+            var frontCube = BABYLON.Mesh.CreateBox("frontCube", 10, this.scene);
+            frontCube.position.z += ground._height / 2;
+            frontCube.position.y = 5;
+            frontCube.scaling.x = 10;
+            frontCube.scaling.z = 0.1;
         };
         return Main;
     }());
--- foot/main.ts
+++ foot/main.ts
@@ -21,10 +21,10 @@ module BABYLON {
             camera.setTarget(new Vector3(0, 0, 0));
 
             // Create light
-            var light = new PointLight("light", new Vector3(1000, 1000, 1000), this.scene);
+            var light = new PointLight("light", new Vector3(0, 100, 0), this.scene);
 
             // Create scene meshes
-            var ground = Mesh.CreateGround("ground", 100, 50, 2, this.scene);
+            var ground = <GroundMesh> Mesh.CreateGround("ground", 100, 50, 2, this.scene);
 
             // Create standard material
             var groundMaterial = new StandardMaterial("ground", this.scene);
@@ -34,7 +34,43 @@ module BABYLON {
             grassTexture.uScale = grassTexture.vScale = 10.0;
             groundMaterial.diffuseTexture = grassTexture;
 
+            groundMaterial.diffuseColor = Color3.Yellow();
             groundMaterial.specularColor = Color3.Black(); // new Color3(0, 0, 0);
+
+            // Skybox
+            var skybox = Mesh.CreateBox("skybox", 1000, this.scene, false, Mesh.BACKSIDE);
+
+            var skyboxMaterial = new StandardMaterial("skyboxMaterial", this.scene);
+            skyboxMaterial.reflectionTexture = new CubeTexture("assets/TropicalSunnyDay", this.scene);
+            skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+            skyboxMaterial.disableLighting = true;
+
+            skybox.material = skyboxMaterial;
+
+            // Create obstacles
+            var leftCube = Mesh.CreateBox("leftCube", 10, this.scene);
+            leftCube.position.x -= ground._width / 2;
+            leftCube.position.y = 5;
+            leftCube.scaling.z = 5;
+            leftCube.scaling.x = 0.1;
+
+            var rightCube = Mesh.CreateBox("rightCube", 10, this.scene);
+            rightCube.position.x += ground._width / 2; // Same as left cube except +ground._height
+            rightCube.position.y = 5;
+            rightCube.scaling.z = 5;
+            rightCube.scaling.x = 0.1;
+
+            var backCube = Mesh.CreateBox("backCube", 10, this.scene);
+            backCube.position.z -= ground._height / 2;
+            backCube.position.y = 5;
+            backCube.scaling.x = 10;
+            backCube.scaling.z = 0.1;
+
+            var frontCube = Mesh.CreateBox("frontCube", 10, this.scene);
+            frontCube.position.z += ground._height / 2;
+            frontCube.position.y = 5;
+            frontCube.scaling.x = 10;
+            frontCube.scaling.z = 0.1;
         }
     }
 }
