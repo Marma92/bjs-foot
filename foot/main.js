9194834eb2175f13103b003082865d37d230f94f
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-02-01 17:30:06 +0100

    Added ball (sphere)

--- foot/main.js
+++ foot/main.js
@@ -2,9 +2,19 @@ var BABYLON;
 (function (BABYLON) {
     var Main = (function () {
         function Main(scene) {
+            // Private members
+            this._ground = null;
+            this._ball = null;
+            this._skybox = null;
+            this._obstacles = [];
+            this._light = null;
             this.scene = scene;
         }
-        Main.prototype.main = function () {
+        // Setups collisions on objects and camera
+        Main.prototype.setupCollisions = function () {
+        };
+        // Setups the meshes used to play football
+        Main.prototype.createMeshes = function () {
             // Create camera
             var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(15, 15, 15), this.scene);
             camera.attachControl(this.scene.getEngine().getRenderingCanvas());
@@ -15,45 +25,49 @@ var BABYLON;
             camera.keysRight = [68]; // D
             camera.setTarget(new BABYLON.Vector3(0, 0, 0));
             // Create light
-            var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 100, 0), this.scene);
+            this._light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 100, 0), this.scene);
             // Create scene meshes
-            var ground = BABYLON.Mesh.CreateGround("ground", 100, 50, 2, this.scene);
+            this._ground = BABYLON.Mesh.CreateGround("ground", 100, 50, 2, this.scene);
             // Create standard material
             var groundMaterial = new BABYLON.StandardMaterial("ground", this.scene);
-            ground.material = groundMaterial;
+            this._ground.material = groundMaterial;
             var grassTexture = new BABYLON.Texture("assets/grass.jpg", this.scene, false, false, BABYLON.Texture.NEAREST_SAMPLINGMODE);
             grassTexture.uScale = grassTexture.vScale = 10.0;
             groundMaterial.diffuseTexture = grassTexture;
             groundMaterial.diffuseColor = BABYLON.Color3.Yellow();
             groundMaterial.specularColor = BABYLON.Color3.Black(); // new Color3(0, 0, 0);
             // Skybox
-            var skybox = BABYLON.Mesh.CreateBox("skybox", 1000, this.scene, false, BABYLON.Mesh.BACKSIDE);
+            this._skybox = BABYLON.Mesh.CreateBox("skybox", 1000, this.scene, false, BABYLON.Mesh.BACKSIDE);
             var skyboxMaterial = new BABYLON.StandardMaterial("skyboxMaterial", this.scene);
             skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/TropicalSunnyDay", this.scene);
             skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
             skyboxMaterial.disableLighting = true;
-            skybox.material = skyboxMaterial;
+            this._skybox.material = skyboxMaterial;
             // Create obstacles
             var leftCube = BABYLON.Mesh.CreateBox("leftCube", 10, this.scene);
-            leftCube.position.x -= ground._width / 2;
+            leftCube.position.x -= this._ground._width / 2;
             leftCube.position.y = 5;
             leftCube.scaling.z = 5;
             leftCube.scaling.x = 0.1;
             var rightCube = BABYLON.Mesh.CreateBox("rightCube", 10, this.scene);
-            rightCube.position.x += ground._width / 2; // Same as left cube except +ground._height
+            rightCube.position.x += this._ground._width / 2; // Same as left cube except +this._ground._height
             rightCube.position.y = 5;
             rightCube.scaling.z = 5;
             rightCube.scaling.x = 0.1;
             var backCube = BABYLON.Mesh.CreateBox("backCube", 10, this.scene);
-            backCube.position.z -= ground._height / 2;
+            backCube.position.z -= this._ground._height / 2;
             backCube.position.y = 5;
             backCube.scaling.x = 10;
             backCube.scaling.z = 0.1;
             var frontCube = BABYLON.Mesh.CreateBox("frontCube", 10, this.scene);
-            frontCube.position.z += ground._height / 2;
+            frontCube.position.z += this._ground._height / 2;
             frontCube.position.y = 5;
             frontCube.scaling.x = 10;
             frontCube.scaling.z = 0.1;
+            this._obstacles = [leftCube, rightCube, backCube, frontCube];
+            // Create ball
+            this._ball = BABYLON.Mesh.CreateSphere("ball", 16, 1, this.scene);
+            this._ball.position.y = 5;
         };
         return Main;
     }());
--- foot/main.ts
+++ foot/main.ts
@@ -3,11 +3,25 @@ module BABYLON {
         // Public members
         public scene: Scene;
 
+        // Private members
+        private _ground: GroundMesh = null;
+        private _ball: Mesh = null;
+        private _skybox: Mesh = null;
+        private _obstacles: Mesh[] = [];
+
+        private _light: PointLight = null;
+
         constructor (scene: Scene) {
             this.scene = scene;
         }
 
-        public main (): void {
+        // Setups collisions on objects and camera
+        public setupCollisions (): void {
+            
+        }
+
+        // Setups the meshes used to play football
+        public createMeshes (): void {
             // Create camera
             var camera = new FreeCamera("camera", new Vector3(15, 15, 15), this.scene);
             camera.attachControl(this.scene.getEngine().getRenderingCanvas());
@@ -21,14 +35,14 @@ module BABYLON {
             camera.setTarget(new Vector3(0, 0, 0));
 
             // Create light
-            var light = new PointLight("light", new Vector3(0, 100, 0), this.scene);
+            this._light = new PointLight("light", new Vector3(0, 100, 0), this.scene);
 
             // Create scene meshes
-            var ground = <GroundMesh> Mesh.CreateGround("ground", 100, 50, 2, this.scene);
+            this._ground = <GroundMesh> Mesh.CreateGround("ground", 100, 50, 2, this.scene);
 
             // Create standard material
             var groundMaterial = new StandardMaterial("ground", this.scene);
-            ground.material = groundMaterial;
+            this._ground.material = groundMaterial;
 
             var grassTexture = new Texture("assets/grass.jpg", this.scene, false, false, Texture.NEAREST_SAMPLINGMODE);
             grassTexture.uScale = grassTexture.vScale = 10.0;
@@ -38,39 +52,45 @@ module BABYLON {
             groundMaterial.specularColor = Color3.Black(); // new Color3(0, 0, 0);
 
             // Skybox
-            var skybox = Mesh.CreateBox("skybox", 1000, this.scene, false, Mesh.BACKSIDE);
+            this._skybox = Mesh.CreateBox("skybox", 1000, this.scene, false, Mesh.BACKSIDE);
 
             var skyboxMaterial = new StandardMaterial("skyboxMaterial", this.scene);
             skyboxMaterial.reflectionTexture = new CubeTexture("assets/TropicalSunnyDay", this.scene);
             skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
             skyboxMaterial.disableLighting = true;
 
-            skybox.material = skyboxMaterial;
+            this._skybox.material = skyboxMaterial;
 
             // Create obstacles
             var leftCube = Mesh.CreateBox("leftCube", 10, this.scene);
-            leftCube.position.x -= ground._width / 2;
+            leftCube.position.x -= this._ground._width / 2;
             leftCube.position.y = 5;
             leftCube.scaling.z = 5;
             leftCube.scaling.x = 0.1;
 
             var rightCube = Mesh.CreateBox("rightCube", 10, this.scene);
-            rightCube.position.x += ground._width / 2; // Same as left cube except +ground._height
+            rightCube.position.x += this._ground._width / 2; // Same as left cube except +this._ground._height
             rightCube.position.y = 5;
             rightCube.scaling.z = 5;
             rightCube.scaling.x = 0.1;
 
             var backCube = Mesh.CreateBox("backCube", 10, this.scene);
-            backCube.position.z -= ground._height / 2;
+            backCube.position.z -= this._ground._height / 2;
             backCube.position.y = 5;
             backCube.scaling.x = 10;
             backCube.scaling.z = 0.1;
 
             var frontCube = Mesh.CreateBox("frontCube", 10, this.scene);
-            frontCube.position.z += ground._height / 2;
+            frontCube.position.z += this._ground._height / 2;
             frontCube.position.y = 5;
             frontCube.scaling.x = 10;
             frontCube.scaling.z = 0.1;
+
+            this._obstacles = [leftCube, rightCube, backCube, frontCube];
+
+            // Create ball
+            this._ball = Mesh.CreateSphere("ball", 16, 1, this.scene);
+            this._ball.position.y = 5;
         }
     }
 }
