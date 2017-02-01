567ceeb9bb0d575eec1e65601d0f8c512e7cf2c6
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-02-01 12:56:55 +0100

    Initial commit, ready to foot !

---
+++ foot/main.js
@@ -0,0 +1,14 @@
+var BABYLON;
+(function (BABYLON) {
+    var Main = (function () {
+        function Main(scene) {
+            this.scene = scene;
+        }
+        Main.prototype.main = function () {
+            // Code here :)
+        };
+        return Main;
+    }());
+    BABYLON.Main = Main;
+})(BABYLON || (BABYLON = {}));
+//# sourceMappingURL=main.js.map
---
+++ foot/main.ts
@@ -0,0 +1,14 @@
+module BABYLON {
+    export class Main {
+        // Public members
+        public scene: Scene;
+
+        constructor (scene: Scene) {
+            this.scene = scene;
+        }
+
+        public main (): void {
+            // Code here :)
+        }
+    }
+}
